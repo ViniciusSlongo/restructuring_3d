9560990f942f1905043a1eefc995a847a4469c18
vinicius <vnslongo@gmail.com>
2022-10-19 21:12:43 -0300

    firts commit

---
+++ main.py
@@ -0,0 +1,100 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+# Arrays to store object points and image points from all the images.
+objpoints = []
+imgpoints = []
+images = glob.glob('*.jpg')
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+    # If found, add object points, image points (after refining them)
+    if ret is True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+np.savez('B.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+
+cv.destroyAllWindows()
+
+
+img = cv.imread('teste.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+
+
+# Load previously saved data
+with np.load('B.npz') as X:
+    mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+
+
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    img = cv.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+    return img
+
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((6*7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+
+for fname in glob.glob('teste.jpg'):
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+
+    if ret is True:
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        # Find the rotation and translation vectors.
+        ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
+        # project 3D points to image plane
+        imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        img = draw(img, corners2, imgpts)
+        cv.imshow('img', img)
+        k = cv.waitKey(0) & 0xFF
+        if k == ord('s'):
+            cv.imwrite(fname[:6]+'.png', img)
+cv.destroyAllWindows()
+
+
+'''
+def draw(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+    # draw ground floor in green
+    img = cv.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
+    # draw pillars in blue color
+    for i, j in zip(range(4), range(4, 8)):
+        img = cv.line(img, tuple(imgpts[i]), tuple(imgpts[j]), 255, 3)
+    # draw top layer in red color
+    img = cv.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
+    return img
+'''
+
+axis = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],
+                   [0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
