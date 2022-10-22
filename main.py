cb29caad54a4121c8d24a13dce7681e7bbd07fd3
RenanMussatto1106 <57591456+RenanMussatto1106@users.noreply.github.com>
2022-10-22 09:22:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ for fname in images:
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 np.savez('B.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+input("Clique ENTER para continuar")
 
 cv.destroyAllWindows()
 
@@ -51,11 +52,28 @@ with np.load('B.npz') as X:
     mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
 
 
-def draw(img, corners, imgpts):
+def draw_1(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
-    img = cv.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
-    img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
-    img = cv.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+
+    corner_a = int(corner[0])
+    corner_b = int(corner[1])
+
+    pontos_imagem_1 = tuple(imgpts[0].ravel())
+    pontos_imagem_2 = tuple(imgpts[1].ravel())
+    pontos_imagem_3 = tuple(imgpts[2].ravel())
+
+    img_pt1_a = int(pontos_imagem_1[0])
+    img_pt1_b = int(pontos_imagem_1[1])
+
+    img_pt2_a = int(pontos_imagem_2[0])
+    img_pt2_b = int(pontos_imagem_2[1])
+
+    img_pt3_a = int(pontos_imagem_3[0])
+    img_pt3_b = int(pontos_imagem_3[1])
+
+    img = cv.line(img, (corner_a, corner_b), (img_pt1_a, img_pt1_b), (255, 0, 0), 5)
+    img = cv.line(img, (corner_a, corner_b), (img_pt2_a, img_pt2_b), (0, 255, 0), 5)
+    img = cv.line(img, (corner_a, corner_b), (img_pt3_a, img_pt3_b), (0, 0, 255), 5)
     return img
 
 
@@ -64,26 +82,30 @@ objp = np.zeros((6*7, 3), np.float32)
 objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
 
-for fname in glob.glob('teste.jpg'):
+for fname in glob.glob('calibresult.png'):
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
 
     if ret is True:
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
         # Find the rotation and translation vectors.
         ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
+
         # project 3D points to image plane
         imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
-        img = draw(img, corners2, imgpts)
+        img = draw_1(img, corners2, imgpts)
+
+        # cv.imwrite('resultado.png', img)
         cv.imshow('img', img)
         k = cv.waitKey(0) & 0xFF
         if k == ord('s'):
             cv.imwrite(fname[:6]+'.png', img)
+
 cv.destroyAllWindows()
 
 
-'''
 def draw(img, corners, imgpts):
     imgpts = np.int32(imgpts).reshape(-1, 2)
     # draw ground floor in green
@@ -94,7 +116,7 @@ def draw(img, corners, imgpts):
     # draw top layer in red color
     img = cv.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
     return img
-'''
+
 
 axis = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],
                    [0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
